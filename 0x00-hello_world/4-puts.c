8ce7eeaef0395f87a12705f4ad2cfbaaa43ce98c
MZN-57 <mazenkaram19@gmail.com>
2023-03-16 06:46:14 -0700

    all_2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,11 @@
  * Describtion: Write a C program that prints exactly
  *
  *Return: Always 0 (Success)
- * */
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0 ;
+	return 0;
 }
