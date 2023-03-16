e2170d2acebc7918f06bfebef153cdad833c53ed
MZN-57 <mazenkaram19@gmail.com>
2023-03-16 06:44:14 -0700

    all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: Write a C program that prints exactly
+ *
+ *Return: Always 0 (Success)
+ * */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0 ;
+}
