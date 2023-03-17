0c103797933b4fe53d5fc0ec8c94824eeb8926ff
MZN-57 <mazenkaram19@gmail.com>
2023-03-17 16:15:00 -0700

    4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lh;
+
+	for (lh = 'a'; lh <= 'z'; lh++)
+		putchar(lh);
+	for (lh = 'A'; lh <= 'Z'; lh++)
+		putchar(lh);
+	putchar('\n');
+	return (0);
+}
