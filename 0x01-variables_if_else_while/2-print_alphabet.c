68f2ef2703e949d69f7538e6631a09d815a8ca7e
MZN-57 <mazenkaram19@gmail.com>
2023-03-17 16:11:21 -0700

    3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery point,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
