5f4086bdc070ce772332d19e5cf2376767c99eea
MZN-57 <mazenkaram19@gmail.com>
2023-03-17 16:20:29 -0700

    8

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
