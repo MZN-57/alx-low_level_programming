4a905df199bc2055511c58128776560ff40884b5
MZN-57 <mazenkaram19@gmail.com>
2023-03-17 16:21:42 -0700

    9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
