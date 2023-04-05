d769ce842b81bed30b4c801202ff4a2904903075
MZN-57 <mazenkaram19@gmail.com>
2023-04-04 19:18:02 -0700

    hello

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,28 @@
 #include "main.h"
 /**
  * _strspn - Entry point
- *
  * @s: an inputed data
- *
  * @accept: an inputed data
- *
  * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int m = 0;
+	unsigned int n = 0;
 	int q;
 
 	while (*s)
 	{
-		for (q = 0; accept[q]; r++)
+		for (q = 0; accept[q]; q++)
 		{
 			if (*s == accept[q])
 			{
-				m++;
+				n++;
 				break;
 			}
 			else if (accept[q + 1] == '\0')
-				return (m);
+				return (n);
 		}
 		s++;
 	}
-	return (m);
+	return (n);
 }
