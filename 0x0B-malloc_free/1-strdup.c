6e0612f9781938de35ef0fc1270c28ddeb0ce209
MZN-57 <mazenkaram19@gmail.com>
2023-04-11 19:05:53 -0700

    hope

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,22 +10,22 @@
  */
 char *_strdup(char *original_string)
 {
-    char *new_string;
-    int original_length = 0, i = 0;
+	char *new_string;
+	int original_length = 0, i = 0;
 
-    if (original_string == NULL)
-        return NULL;
+	if (original_string == NULL)
+		return NULL;
 
-    while (original_string[original_length] != '\0')
-        original_length++;
+	while (original_string[original_length] != '\0')
+		original_length++;
 
-    new_string = malloc(sizeof(char) * (original_length + 1));
+	new_string = malloc(sizeof(char) * (original_length + 1));
 
-    if (new_string == NULL)
-        return NULL;
+	if (new_string == NULL)
+		return NULL;
 
-    for (i = 0; original_string[i]; i++)
-        new_string[i] = original_string[i];
+	for (i = 0; original_string[i]; i++)
+		new_string[i] = original_string[i];
 
-    return new_string;
+	return new_string;
 }
