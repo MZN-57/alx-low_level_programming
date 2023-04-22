87f7cc56e3dbafb6fadb7faa0a4ed5f54f3a280d
MZN-57 <mazenkaram19@gmail.com>
2023-04-21 18:46:01 -0700

    hope

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,15 +1,14 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
- * struct op - A struct that maps an operator to an arithmetic function.
- * @op: The operator.
- * @f: The associated function.
- **/
+ * struct op -  struct op.
+ * @op:  operator.
+ * @f: associated function.
+ */
 typedef struct op
 {
-	char *operator;
-	int (*function)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -17,6 +16,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *operator))(int, int);
+int (*get_op_func(char *s))(int, int);
 
 #endif
